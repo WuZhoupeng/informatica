cb8964c902a047c90808c753513a9620459a213c
WuZhoupeng <237608388+WuZhoupeng@users.noreply.github.com>
2026-01-12 09:51:58 +0000

    Data 12/01/26: ora di sistemi

--- teoria-esercitazione/es_post_natale/main.c
+++ teoria-esercitazione/es_post_natale/main.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
-void insertText (char** full_text, const char* text);
-void addText(char** full_text, const char* text);
+void insertText (char** full_text, char* text, int* pos);
+void addText(char** full_text, char* text);
 void undo (char** full_text, char** prev_text);
 void redo (char** full_text, char** prev_text);
-void saveOnFile (const char** full_text, const char* filename);
+void saveOnFile (const char** full_text, char* filename);
 
 int main () {
 
 }
 
-void insertText (char** full_text, const char* text) {
+void insertText (char** full_text, char* text, int* pos) {
+    if (*full_text == NULL) {
+        *full_text = text;
+    }
 
+    char* temp = *full_text;
+
+    if (pos != NULL) {
+        if (strlen(*full_text) < *pos) {
+            for (int i = 0; (*full_text + i) != NULL; ++i) {}
+
+            *full_text = text;
+
+            return;
+        }
+        else {
+            for (int i = strlen(*full_text); i > *pos; --i) {
+                *(*full_text + i + 1) = *(*full_text + i);
+            }
+        }
+    }
 }
 
-void addText(char** full_text, const char *text) {
+void addText(char** full_text, char *text) {
 
 }
 
@@ -26,6 +46,6 @@ void redo (char** full_text, char** prev_text) {
 
 }
 
-void saveOnFile (const char** full_text, const char* filename) {
+void saveOnFile (const char** full_text, char* filename) {
     
 }
